cb87690f9dc057f05593e42087c31062bf7b6358
Richard Chimama <rich.chime@gmail.com>
2025-03-09 21:15:39 +0100

    fixed lint

--- components/ServiceSection/InfoSection/style.ts
+++ components/ServiceSection/InfoSection/style.ts
@@ -1,18 +1,18 @@
 import { Box, styled, Typography } from "@mui/material";
 import Theme from "../../../theme";
 
-export const Container = styled(Box)(prop => ({
+export const Container = styled(Box)(() => ({
     display:'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     gap: '10px',
 }))
 
-export const Wrapper = styled(Box)(prop =>({
+export const Wrapper = styled(Box)(() =>({
     width: '70%',
 }))
 
-export const IconWrapper = styled(Box)(prop =>({
+export const IconWrapper = styled(Box)(() =>({
     backgroundColor: Theme.colors.secondary,
     borderRadius: '8px',
     height: '75px',
@@ -22,12 +22,12 @@ export const IconWrapper = styled(Box)(prop =>({
     alignItems: 'center',
 }))
 
-export const HeadLabel = styled(Typography)(prop =>({
+export const HeadLabel = styled(Typography)(() =>({
     fontSize: '30px',
     fontWeight: 600,
     color: Theme.colors.secondary,
 }))
 
-export const LabelText = styled(Typography)(prop =>({
+export const LabelText = styled(Typography)(() =>({
     color: Theme.colors.secondary,
 }))
